d489b79d86b3286e8c68ca76abc76a136c2c7408
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-07 16:44:21 +0530

    feat/- connecting to mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
-import config from './config/config.js'
-import app from './express.js'
+import config from "./config/config.js";
+import app from "./express.js";
+import mongoose from "mongoose";
 
-app.listen(config.port,(err)=>{
-    if(err){
-        console.log(err)
-    }
-    console.log(`Server is running on the port ${config.port}`)
-})
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected successfully");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB connection error:", err);
+});
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server is running on port ${config.port}`);
+});
