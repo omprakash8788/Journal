4eeaf3d2e5c11f0314aa876416c56347dceaed4f
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-07 15:46:57 +0530

    setup express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-// Setup - 
+import config from './config/config.js'
+import app from './express.js'
+
+app.listen(config.port,(err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log(`Server is running on the port ${config.port}`)
+})
