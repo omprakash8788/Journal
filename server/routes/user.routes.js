af1fcd9847319672e0da081bc5e96cbac57e0097
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-08 11:51:06 +0530

    sign in , sing out and crud operation

---
+++ server/routes/user.routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import userCtrl from "../controllers/user.controller.js";
+import authCtrl from "../controllers/auth.controller";
+
+const router = express.Router();
+
+router.route("/api/users").get(userCtrl.list).post(userCtrl.create);
+
+router
+  .route("/api/users/:userId")
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(
+    authCtrl.requireSignin,
+    authCtrl.hasAuthorization,
+    userCtrl,
+    userCtrl.remove,
+  );
+
+router.param("userId", userCtrl.userByID);
+
+export default router;
