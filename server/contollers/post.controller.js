d29dd7a7e197a4e2f56969c067c950256357d72f
omprakash8788 <omprakashkumar552@gmail.com>
2026-03-16 15:49:48 +0530

    adding and writing comment api logic

--- server/contollers/post.controller.js
+++ server/contollers/post.controller.js
@@ -137,6 +137,26 @@ const unlike = async (req, res) => {
   }
 };
 
+const comment = async (req, res) => {
+  let comment = req.body.comment;
+  comment.postedBy = req.body.userId;
+  try {
+    let result = await Post.findByIdAndUpdate(
+      req.body.postId,
+      { $push: { comments: comment } },
+      { new: true },
+    )
+      .populate("comments.postedBy", "_id name")
+      .populate("postedBy", "_id name")
+      .exec();
+    res.json(result);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+
 export default {
   listNewsFeed,
   isPoster,
@@ -146,5 +166,6 @@ export default {
   photo,
   postByID,
   like,
-  unlike
+  unlike,
+  comment
 };
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route("/api/posts/like").put(authCtrl.requireSignin, postCtrl.like);
 router.route("/api/posts/unlike").put(authCtrl.requireSignin, postCtrl.unlike);
+router.route("/api/posts/comment").put(authCtrl.requireSignin, postCtrl.comment);
 
 router
   .route("/api/posts/feed/:userId")
